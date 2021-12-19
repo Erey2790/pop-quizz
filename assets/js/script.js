bad3681819733695d5f395ee9468c237e6cdb9d7
ElmerPC <erey2790@gmail.com>
2021-12-19 10:06:10 -0600

    first functions added start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+var startButton = document.getElementById('start-button')
+
+startButton.addEventListener("click", startGame)
+
+
+
+//START GAME
 function startGame() {
-    
+    console.log("button works")
 }
+
+//CONTINUE QUESTION
+function continueQuestion() {
+
+}
+
+//SELECT ANSWER
+function selectAnswer() {
+
+}
+
